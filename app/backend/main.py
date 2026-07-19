8cd976f0e653ad0df13d8ed8be35b48d6cba8a76
Sourav Das <souravdas5675@gmail.com>
2026-07-19 17:17:10 +0530

    Allow Vercel frontend

--- app/backend/main.py
+++ app/backend/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://your-vercel-app.vercel.app"
+        "https://web-based-isl-interpreter.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
