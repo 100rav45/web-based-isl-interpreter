1e56a44e68b00211d7b1653697ecbfcb5c664157
Sourav Das <souravdas5675@gmail.com>
2026-07-19 17:36:26 +0530

    Temporarily allow all origins for CORS testing

--- app/backend/main.py
+++ app/backend/main.py
@@ -17,11 +17,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://web-based-isl-interpreter.vercel.app"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
